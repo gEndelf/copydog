423c24e80e5f671d91c9f11026c69c8a23ecb924
gEndelf <alex.com.ua@gmail.com>
2014-09-14 15:16:28 +0400

    extract requirements from setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,13 @@ from setuptools import setup
 from copydog import __version__
 
 
+requirements = open('requirements.txt').read().split('\n')
+
 setup(
     name='copydog',
     version=__version__,
     packages=['copydog', 'copydog.api', 'copydog.utils'],
-    install_requires=[
-        'requests==0.13.6',
-        'redis==2.6.2',
-        'python-dateutil==2.1',
-        'pytz==2012c',
-        'docopt==0.4.1',
-        'PyYAML==3.10',
-        'python-daemon==1.6',
-        'texttable==0.8.1',
-    ],
-    test_requires=[
-        'nose==1.1.2',
-        'mock==1.0b1',
-        'sphinx==1.1.3',
-        'tox==1.4.2',
-    ],
+    install_requires=requirements,
     entry_points={
         "console_scripts": [
             'copydog = copydog.bin.runner:main'
